c26c4382a5f4274e69593eef01325c8adea7bf37
Herve Bourelle <herve.bourelle@gmail.com>
2019-12-04 17:42:23 +0100

    modification conf.js

--- back/conf.js
+++ back/conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  'Hasselblad3', // le mot de passe
+password :  '', // le mot de passe
 database :  'tuktuktravel', // le nom de la base de données
 });
 module.exports = connection;
