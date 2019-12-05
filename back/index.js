91993c55f17a8b096ba44c969c44f19cee2de6af
Herve Bourelle <herve.bourelle@gmail.com>
2019-12-05 11:40:18 +0100

    road users done

--- back/index.js
+++ back/index.js
@@ -1,3 +1,4 @@
+const connection = require('./conf');
 const express = require('express');
 const app = express();
 const port = 8000;
@@ -5,6 +6,17 @@ const port = 8000;
 app.get('/', (request, response) => {
   response.send('Bienvenue sur Express');
 });
+
+app.get('/api/users', (req, res) => {
+  connection.query('SELECT * from users', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des users');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 
 app.listen(port, (err) => {
   if (err) {
