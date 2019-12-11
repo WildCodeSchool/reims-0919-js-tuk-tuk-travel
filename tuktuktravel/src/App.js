e32c274be343bf2fae162be3623fb947e3fbef2a
Maxime Féry <maxime.fery@outlook.fr>
2019-12-11 17:25:24 +0100

    correction call

--- tuktuktravel/src/App.js
+++ tuktuktravel/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <FormUsers />
         <TravelCards />
-        < TravelForm />
+        <TravelForm />
       </header>
     </div>
   );
