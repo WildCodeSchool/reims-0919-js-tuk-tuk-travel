ecbe333205e3fdb89db581c284e9beaa99c52b1c
XeniaR1S <lecomtep.x@gmail.com>
2019-12-11 17:14:18 +0100

    pull dev

--- tuktuktravel/src/App.js
+++ tuktuktravel/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import TravelCards from './components/TravelCards'
+import UserConnexion from './components/UserConnexion';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TravelCards />
+        <UserConnexion />
       </header>
     </div>
   );
