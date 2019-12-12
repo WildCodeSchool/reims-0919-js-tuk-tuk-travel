f8cb1f3f49acc9bfbb4e3eb721a9f5828fc063af
Sylvie64 <sylvie.lecrocq64@gmail.com>
2019-12-12 15:58:44 +0100

    add file home with logo

--- tuktuktravel/src/App.js
+++ tuktuktravel/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import TravelCards from './components/TravelCards'
 import FormUsers from './components/FormUsers'
 import TravelForm from './components/TravelForm'
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home />        
         <FormUsers />
         <TravelCards />
-        < TravelForm />
+        <TravelForm />
       </header>
     </div>
   );
