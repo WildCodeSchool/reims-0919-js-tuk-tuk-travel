64c42f1fa3fe5e4171b42399e223274a5aca8798
XeniaR1S <lecomtep.x@gmail.com>
2020-01-09 14:47:11 +0100

    test list of country

--- tuktuktravel/src/components/Home.js
+++ tuktuktravel/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import myLogo from '../img/tuk-tuk logo.png';
 import { Link } from 'react-router-dom';
 import '../App.css'
+import CountryList from './CountryList';
 
 const Home = () => {
 
@@ -17,6 +18,9 @@ const Home = () => {
         <Link to="/formusers" className='enter-link'>S'inscrire</Link>
 
         <Link to="/userconnexion">Se connecter</Link>
+
+        <CountryList/>
+
 
         {/* <Link to="/travelform">Créer un voyage</Link> */}
 
