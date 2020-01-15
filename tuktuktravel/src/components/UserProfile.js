5ee94dabceb74a79956fe65283e1d889756a602b
XeniaR1S <lecomtep.x@gmail.com>
2020-01-15 11:48:17 +0100

    add style

--- tuktuktravel/src/components/UserProfile.js
+++ tuktuktravel/src/components/UserProfile.js
@@ -51,37 +51,78 @@ class UserProfile extends Component {
         <div className="title-form-user">PROFIL</div>
         
           <form className='add-user' onSubmit={this.submit} >
-            
-            <label htmlFor="lastname">Nom</label>
-            <input type="text" id="lastname" onChange={this.change} />
-            <figure className='logo-modify'>
-              <img className='img-logo-modify' src={logoAccueil} alt='Modify'/>
-            </figure>
-            
-            <label htmlFor="firstname">Prénom</label>
-            <input type="text" id="firstname" onChange={this.change} />
-            
-            <label htmlFor="password">Mot de passe</label>
-            <input type="text" id="password" onChange={this.change} />
-            
-            <label htmlFor="birthday">Date de naissance</label>
-            <input type="date" id="birthday" onChange={this.change} />
-            
-            <label htmlFor="countrys">Pays</label>
-            <CountryList country={this.state.country} change={this.change} id='countrys' />
-            
-            <label htmlFor="city">Ville</label>
-            <input type="text" id="city" onChange={this.change} />
-            
-            <label htmlFor="email">E-mail</label>
-            <input type="text" id="email" onChange={this.change} />
-            
-            <label htmlFor="phone_number">Numéro de téléphone</label>
-            <input type="text" id="phone_number" onChange={this.change} />
-            
-            <label htmlFor="description">Description</label>
-            <input type="text" id="description" onChange={this.change} />
-            
+            <div className='modify-bloc'>
+              <label htmlFor="lastname">Nom</label>
+              <input type="text" id="lastname" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="firstname">Prénom</label>
+              <input type="text" id="firstname" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="password">Mot de passe</label>
+              <input type="text" id="password" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="birthday">Date de naissance</label>
+              <input type="date" id="birthday" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="countrys">Pays</label>
+              <CountryList country={this.state.country} change={this.change} id='countrys' />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="city">Ville</label>
+              <input type="text" id="city" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="email">E-mail</label>
+              <input type="text" id="email" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="phone_number">Numéro de téléphone</label>
+              <input type="text" id="phone_number" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
+            <div className='modify-bloc'>
+              <label htmlFor="description">Description</label>
+              <input type="text" id="description" onChange={this.change} />
+              <figure className='logo-modify'>
+                <img className='img-logo-modify' src={logoModify} alt='Modify'/>
+              </figure>
+            </div>
+
             <button className='send-form-users'>Envoyer</button>
           
           </form>
