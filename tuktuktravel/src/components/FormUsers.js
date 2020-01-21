4a888a877ad03c33771aa8a112e71f04a5376965
XeniaR1S <lecomtep.x@gmail.com>
2020-01-21 11:35:22 +0100

    Add arrow back in Form User

--- tuktuktravel/src/components/FormUsers.js
+++ tuktuktravel/src/components/FormUsers.js
@@ -16,6 +16,7 @@ class FormUsers extends Component {
       firstname: '',
       sex: '',
       password: '',
+      comfirm_password: '',
       birthday: '',
       country: '',
       city: '',
@@ -23,8 +24,25 @@ class FormUsers extends Component {
       phone_number: '',
       description: '',
       avatar: '',
+      firstSection: true,
+      secondSection: false
     };
   }
+
+  nextPage = () => {
+    this.setState({
+      firstSection: false,
+      secondSection: true
+    })
+  } 
+
+  previousPage = () => {
+    this.setState({
+      firstSection: true,
+      secondSection: false
+    })
+  } 
+
 
   change = e => {
     this.setState({
@@ -50,45 +68,81 @@ class FormUsers extends Component {
 
   render(){
     return(
+      
       <div className='form-users'>
-        <div className="title-form-user">INFOS PERSONNELLES</div>
-        <Link className='link-cgu-to-formuser' to="/Home">
-          <figure className='fig-back-arrow'>
-            <img className='back-arrow' src={back} alt='Arrow to back'/>
-          </figure>
-        </Link>
-        <form className='add-user' onSubmit={this.submit} >
-          <label htmlFor="lastname">Nom</label>
-          <input type="text" id="lastname" onChange={this.change} />
-          <label htmlFor="firstname">Prénom</label>
-          <input type="text" id="firstname" onChange={this.change} />
-          <label htmlFor="sex">Sexe</label>
-          <div className='sex-form-user'>
-            <select className="sex sex-form-user" id="sex" onChange={this.change} >
-            <option value=""></option>
-            <option value="homme">Homme</option>
-            <option value="femme">Femme</option>
-            <option value="autre">Autre</option>
-            </select>
+        {this.state.firstSection?
+        <div>
+          <div className="title-form-user">INFOS PERSONNELLES</div>
+          <Link className='link-back-arrow' to="/Home">
+            <figure className='fig-back-arrow'>
+              <img className='back-arrow' src={back} alt='Arrow to back'/>
+            </figure>
+          </Link>
+          
+          <form className='add-user' onSubmit={this.submit} />
+            <div className='firstSection'>
+              <div>
+                <label htmlFor="lastname">Nom</label>
+                <input type="text" id="lastname" onChange={this.change} />
+
+                <label htmlFor="firstname">Prénom</label>
+                <input type="text" id="firstname" onChange={this.change} />
+
+                <label htmlFor="sex">Sexe</label>
+                <div className='sex-form-user'>
+                  <select className="sex sex-form-user" id="sex" onChange={this.change} >
+                  <option value=""></option>
+                  <option value="homme">Homme</option>
+                  <option value="femme">Femme</option>
+                  <option value="autre">Autre</option>
+                  </select>
+                </div>
+
+                <label htmlFor="password">Mot de passe</label>
+                <input type="password" id="password" onChange={this.change} />
+                <label htmlFor="comfirm-password"> Comfirmation de Mot de passe</label>
+                <input type="password" id="password" onChange={this.change} />
+
+                <label htmlFor="birthday">Date de naissance</label>
+                <input type="date" id="birthday" onChange={this.change} />
+
+                <div className='nextButton'>
+                  <button onClick = {this.nextPage}>Suivant</button>
+                </div>
+              </div>
+            </div>
+        </div>
+        :
+        <div>
+          <button onClick = {this.previousPage}>
+            <figure className='fig-back-arrow'>
+              <img className='back-arrow' src={back} alt='Arrow to back'/>
+            </figure>
+          </button>
+
+          <div className='secondSection'>
+            <label htmlFor="countrys">Pays</label>
+            <CountryList country={this.state.country} change={this.change} />
+
+            <label htmlFor="city">Ville</label>
+            <input type="text" id="city" onChange={this.change} />
+            
+            <label htmlFor="email">E-mail</label>
+            <input type="text" id="email" onChange={this.change} />
+            
+            <label htmlFor="phone_number">Numéro de téléphone</label>
+            <input type="text" id="phone_number" onChange={this.change} />
+            
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" onChange={this.change} />
+            
+            <Link  to="/cgu">Conditions générales d'utilisation</Link>
+            
+            <button className='send-form-users' onClick={this.submit}>Envoyer</button>
           </div>
-          <label htmlFor="password">Mot de passe</label>
-          <input type="password" id="password" onChange={this.change} />
-          <label htmlFor="birthday">Date de naissance</label>
-          <input type="date" id="birthday" onChange={this.change} />
-          <label htmlFor="countrys">Pays</label>
-          <CountryList country={this.state.country} change={this.change} />
-          <label htmlFor="city">Ville</label>
-          <input type="text" id="city" onChange={this.change} />
-          <label htmlFor="email">E-mail</label>
-          <input type="text" id="email" onChange={this.change} />
-          <label htmlFor="phone_number">Numéro de téléphone</label>
-          <input type="text" id="phone_number" onChange={this.change} />
-          <label htmlFor="description">Description</label>
-          <input type="text" id="description" onChange={this.change} />
-          <Link  to="/cgu">Conditions générales d'utilisation</Link>
-          <button className='send-form-users'>Envoyer</button>
-        </form>
-        <UploadAvatar />
+          
+          <UploadAvatar />
+        </div>}
       </div>
     );
   }
@@ -99,5 +153,4 @@ function  mapStateToProps(state) {
       avatar:  state.avatar.avatar,
   }
 };
-
 export  default  connect(mapStateToProps)(FormUsers)
